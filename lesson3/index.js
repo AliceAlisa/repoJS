6d7141e0def7966b14662c56a1473d100221c6e7
alicew <wanderlust.alice00@gmail.com>
2021-08-27 14:35:40 +0200

    basic Home Work 3 completed

--- lesson3/index.js
+++ lesson3/index.js
@@ -56,17 +56,13 @@ class ProductItem {
 
 let list = new ProductsList();
 
-/*class CartList extends ProductsList {
-
-}*/
 class CartList {
     constructor(container = '.cart') {
         this.container = container;
-        this.goods = [];//массив товаров из JSON документа
+        this.goods = [];
         this.sumCart = 0;
-        this.isClose = true;
         this._getProducts()
-            .then(data => { //data - объект js
+            .then(data => {
                 this.goods = [...data.contents];
                 this.render()
             });
@@ -84,28 +80,18 @@ class CartList {
 
     render() {
         const block = document.querySelector(this.container);
-        block.insertAdjacentHTML("beforeend", `<h2>Список товаров корзины:</h2>`);
         for (let product of this.goods) {
             const item = new CartItem(product);
             block.insertAdjacentHTML("beforeend", item.render());
         }
         this.countPrice();
-        block.insertAdjacentHTML("beforeend", `<p><span class="deco">Итого</span> сумма корзины: <span class="deco"> ${this.sumCart} $</span></p>`);
-
+        document.querySelector(this.container).insertAdjacentHTML("beforeend", `<p class="price">Итого: <span class="deco">${this.sumCart}$</span></p>`);
     }
 
     buttonClick() {
         document.querySelector('.btn-cart').addEventListener('click', () => {
-            if (this.isClose) {
-                document.querySelector('.cart').classList.add('cart-open');
-                this.isClose = false;
-            } else {
-                document.querySelector('.cart').classList.remove('cart-open');
-                this.isClose = true;
-            }
-
+            document.querySelector(this.container).classList.toggle('cart-open');
         })
-
     }
 
     countPrice() {
@@ -113,6 +99,7 @@ class CartList {
         return this.sumCart;
     }
 }
+
 class CartItem {
     constructor(product, img = 'img/img.jpeg') {
         this.title = product.product_name;
@@ -127,38 +114,9 @@ class CartItem {
                 <h3>${this.title}</h3>
                 <p>${this.price} $ </p>
                 <p>Количество: ${this.amount} шт.</p>
-                <button class="buy-btn">Убрать из корзины</button>
+                <button class="buy-btn">Удалить</button>
             </div>`
     }
 }
 
 let cartlist = new CartList();
-
-/*class Cart {
-    constructor() {
-
-    }
-
-    addToCart() {
-        //данный метод будет добавлять элемент в корзину
-    }
-
-    renderProduct() {
-       // данный метод будет отображать добавленный элемент корзины
-    }
-
-    deleteFromCart() {
-        //данный метод будет удалять элемент из корзины
-    }
-
-    clearAllCart() {
-        //данный метод будет очищать полностью всю корзину
-    }
-}
-
-class ItemCart {
-    constructor() {
-
-    }
-}
-*/
