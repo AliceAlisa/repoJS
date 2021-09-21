28942b9d4d57ee9304348686cd0a8cf3256f7d51
alicew <wanderlust.alice00@gmail.com>
2021-09-21 15:15:55 +0200

    changed filter and cart

--- lesson7/public/js/CartComponent.js
+++ lesson7/public/js/CartComponent.js
@@ -1,37 +1,37 @@
 // const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 Vue.component('cart', {
-    data(){
-      return {
-          cartUrl: '/getBasket.json',
-          cartItems: [],
-          imgCart: 'https://placehold.it/50x100',
-          showCart: false
-      }
+    data() {
+        return {
+            cartUrl: '/getBasket.json',
+            cartItems: [],
+            imgCart: 'https://placehold.it/50x100',
+            showCart: false
+        }
     },
-    mounted(){
+    mounted() {
         this.$parent.getJson(`/api/cart`)
             .then(data => {
-                for (let item of data.contents){
+                for (let item of data.contents) {
                     this.$data.cartItems.push(item);
                 }
             });
     },
     methods: {
-        addProduct(item){
+        addProduct(item) {
             let find = this.cartItems.find(el => el.id_product === item.id_product);
-            if(find){
-                this.$parent.putJson(`/api/cart/${find.id_product}`, {quantity: 1})
+            if (find) {
+                this.$parent.putJson(`/api/cart/${find.id_product}`, { quantity: 1 })
                     .then(data => {
-                        if(data.result === 1){
+                        if (data.result === 1) {
                             find.quantity++
                         }
                     })
             } else {
-                const prod = Object.assign({quantity: 1}, item);
+                const prod = Object.assign({ quantity: 1 }, item);
                 this.$parent.postJson(`/api/cart`, prod)
                     .then(data => {
-                        if(data.result === 1){
+                        if (data.result === 1) {
                             this.cartItems.push(prod)
                         }
                     })
@@ -50,11 +50,11 @@ Vue.component('cart', {
             //         }
             //     })
         },
-        remove(item){
+        remove(item) {
             this.$parent.getJson(`${API}/addToBasket.json`)
                 .then(data => {
                     if (data.result === 1) {
-                        if(item.quantity>1){
+                        if (item.quantity > 1) {
                             item.quantity--;
                         } else {
                             this.cartItems.splice(this.cartItems.indexOf(item), 1);
@@ -64,7 +64,8 @@ Vue.component('cart', {
         },
     },
     template: `<div>
-<button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
+<button class="btn-cart basket logos" type="button" @click="showCart = !showCart"><img src="img/basket.svg" alt="menu photo5">
+<span class="count">5</span></button>
         <div class="cart-block" v-show="showCart">
             <cart-item v-for="item of cartItems" :key="item.id_product" :img="imgCart" :cart-item="item" @remove="remove">
             </cart-item>
@@ -72,6 +73,9 @@ Vue.component('cart', {
         </div>
     `
 });
+/*<a href="cart.html" class="basket logos">
+                                <img src="img/basket.svg" alt="menu photo5">
+                                <span class="count">5</span>*/
 
 Vue.component('cart-item', {
     props: ['img', 'cartItem'],
--- lesson7/public/js/FilterComp.js
+++ lesson7/public/js/FilterComp.js
@@ -1,13 +1,19 @@
 Vue.component('filter-el', {
-    data(){
-      return {
-          userSearch: ''
-      }
+    data() {
+        return {
+            userSearch: ''
+        }
     },
     template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
                 <input type="text" class="search-field" v-model="userSearch">
                 <button type="submit" class="btn-search">
-                    <i class="fas fa-search"></i>
+                <img src="img/loupe.svg" alt="menu photo2">
+                
                 </button>
             </form>`
 })
+
+/*<a href="#">
+    <i class="fas fa-search"></i>
+                            <img src="img/loupe.svg" alt="menu photo2">
+                        </a>*/
